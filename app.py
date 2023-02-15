2bb90bc9c3ab07f3eb2850dd764450dc54bf7413
DevoperMees <56345458+DevoperMees@users.noreply.github.com>
2023-02-15 11:29:05 +0100

    Resolved merge conflicts

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-from flask import Flask, redirect, url_for, request, flash, render_template, jsonify, send_from_directory
+from flask import Flask, redirect, url_for, request, flash, render_template, jsonify, send_from_directory, send_file
 import os.path
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-import mimetypes
-
-
-=======
-from flask import Flask, redirect, url_for, request, flash, render_template, send_file
-import os.path
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
->>>>>>> origin/qrCode
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
 
 LISTEN_ALL = "0.0.0.0"
@@ -34,13 +24,11 @@ db.init_app(app)
 
 # import declared routes
 import routes.authRoutes
-<<<<<<< HEAD
 import routes.lessonRoutes
+import routes.qrRoutes
 routes.lessonRoutes.setup_lesson_routes(app)
 routes.authRoutes.setup_auth_routes(app)
-=======
-import routes.qrRoutes
->>>>>>> origin/qrCode
+
 
 # Secret key for the session
 app.secret_key = '1335eb3948fb7b64a029aa29'
@@ -51,20 +39,12 @@ app.secret_key = '1335eb3948fb7b64a029aa29'
 def start():
     return redirect(url_for('login'))
 
-<<<<<<< HEAD
-=======
-
 # This route will redirect to the login route
->>>>>>> origin/qrCode
 @app.route("/index")
 def index():
     return render_template(
         "index.html", name=current_user.name
     )
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/qrCode
 if __name__ == "__main__":
     app.run(host=FLASK_IP, port=FLASK_PORT, debug=FLASK_DEBUG)
