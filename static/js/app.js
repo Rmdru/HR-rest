96c2ef06fa675b706313da09488fd6135f15f596
Ruben-1056362 <1056362@hr.nl>
2023-02-22 12:57:46 +0100

    HOTFIX: app.js bug fixed

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,7 @@ import Attendance from "./attendance.js";
 document.addEventListener("DOMContentLoaded", () => {
     new Lesson();
     new Attendance();
-  new Student();
-  new Class_();
-  new Teacher();
+    new Student();
+    new Class_();
+    new Teacher();
+})
